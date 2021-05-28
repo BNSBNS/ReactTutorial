36ed1d384108f6991ef95cb5e90252f7d0e8c22b
ubuntuDesktop <y3kmarvin@gmail.com>
2021-05-28 22:11:12 +0800

    another app created

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 
 const element = <h1>Hello world</h1>;
 
-console.log(element);
+
 ReactDOM.render(element, document.getElementById("root"));
 
-console.log(element);
+
+
 //a
 //bb
 //third usin vs code
