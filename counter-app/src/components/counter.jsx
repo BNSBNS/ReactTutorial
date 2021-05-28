8b23deaf061178f966cb36e827e815ece53903b6
ubuntuDesktop <y3kmarvin@gmail.com>
2021-05-29 02:38:57 +0800

    list on counter.jsx

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,19 +3,32 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
     return (
       <div>
         {" "}
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
+        <span className={this.getBadeClasses()}>
           {" "}
           {this.formatCount()}{" "}
         </span>{" "}
         <button className="btn btn-secondary btn-sm">Increment</button>{" "}
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadeClasses() {
+    let classes = "badge m-2 badge-";
+
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
