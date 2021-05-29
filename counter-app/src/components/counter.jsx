0ac69e1a983cc9b3cf56a64ad31af2fdc6325e6d
ubuntuDesktop <y3kmarvin@gmail.com>
2021-05-30 03:09:20 +0800

    new counters.jsx file, added removing buttons/counters

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,30 +2,43 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    value: this.props.counter.value,
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>there are no tags</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}> {tag} </li>
-        ))}
-      </ul>
-    );
-  }
+  handleIncrement = (product) => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
     return (
       <div>
         {" "}
-        {this.state.tags.length === 0 && "Please create new tag!"}{" "}
-        {this.renderTags()}{" "}
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
+        <button
+          onClick={(product) => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
